39d2b9a1354f69da2e8c1e3d7c9b8a0f144d025b
Tariq Yusuf <tariq@tariqyusuf.in>
2014-04-25 20:25:58 -0700

    Changed to a better twitter API, WE HAZ TEH TWEETERZ

--- web.js
+++ web.js
@@ -1,16 +1,12 @@
 var express = require("express");
-var twitter = require("twitter");
+var Twit = require("twit");
 var app = express();
-var twit = new twitter({
+var twitter = new Twit({
 	consumer_key: 'H54AD1rVwqpELtpgvjkZTJLyd',
 	consumer_secret: 'G6mMnu3WLoRB34wdZ73hT9CCvXUMlJF7OiZnLJs4JEnxwJE8SB',
-	access_token_key: '60665636-kQs6rSi6YSl2QZ8sHs2JThXGsvXwWrlBeY1S4Llnd',
+	access_token: '60665636-kQs6rSi6YSl2QZ8sHs2JThXGsvXwWrlBeY1S4Llnd',
 	access_token_secret: 'A1mMbEnPovAwJWMpjoVRraAPkHtYQSF7RGEeoJ00ePy2N'
 });
-
-app.get('/', twit.gatekeeper('/login'), routes.index);
-app.get('/login', routes.login);
-app.get('/twauth', twit.login());
 
 app.get('/', function(req, res) {
     res.send('Hello World!');
@@ -19,4 +15,9 @@ app.get('/', function(req, res) {
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
     console.log("Listening on " + port);
+});
+
+twitter.get('search/tweets', {q: '#messi', count: 1}, function(err, reply) {
+	console.log("Error: " + err);
+	console.log("Reply: " + JSON.stringify(reply));
 });
