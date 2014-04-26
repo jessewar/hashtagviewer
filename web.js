f56229704d669f159afe30781c495a33bcec9e21
Jesse Warren <jesse@jesse.warren>
2014-04-25 20:34:20 -0700

    Home page /query logs the first #query tweet

--- web.js
+++ web.js
@@ -2,14 +2,17 @@ var express = require("express");
 var Twit = require("twit");
 var app = express();
 var twitter = new Twit({
-	consumer_key: 'H54AD1rVwqpELtpgvjkZTJLyd',
-	consumer_secret: 'G6mMnu3WLoRB34wdZ73hT9CCvXUMlJF7OiZnLJs4JEnxwJE8SB',
-	access_token: '60665636-kQs6rSi6YSl2QZ8sHs2JThXGsvXwWrlBeY1S4Llnd',
-	access_token_secret: 'A1mMbEnPovAwJWMpjoVRraAPkHtYQSF7RGEeoJ00ePy2N'
+    consumer_key: 'H54AD1rVwqpELtpgvjkZTJLyd',
+    consumer_secret: 'G6mMnu3WLoRB34wdZ73hT9CCvXUMlJF7OiZnLJs4JEnxwJE8SB',
+    access_token: '60665636-kQs6rSi6YSl2QZ8sHs2JThXGsvXwWrlBeY1S4Llnd',
+    access_token_secret: 'A1mMbEnPovAwJWMpjoVRraAPkHtYQSF7RGEeoJ00ePy2N'
 });
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
+app.get('/:query', function(req, res) {
+    twitter.get('search/tweets', {q: '#' + req.params.query, count: 1}, function(err, reply) {
+        console.log("Error: " + err);
+        console.log("Reply: " + JSON.stringify(reply));
+    });
 });
 
 var port = Number(process.env.PORT || 5000);
@@ -17,7 +20,3 @@ app.listen(port, function() {
     console.log("Listening on " + port);
 });
 
-twitter.get('search/tweets', {q: '#messi', count: 1}, function(err, reply) {
-	console.log("Error: " + err);
-	console.log("Reply: " + JSON.stringify(reply));
-});
